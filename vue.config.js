234f835a99c7ebeee032bdcdd838f57c24a79614
Robert Laverty <roblav96@gmail.com>
2018-03-20 08:41:35 -0400

    migrating deps

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,6 @@ eyes.defaults.maxLength = 65536
 eyes.defaults.showHidden = true
 const webpack = require('webpack')
 const path = require('path')
-const dotenv = require('dotenv')
 const package = require('./package.json')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
@@ -15,14 +14,15 @@ global.NODE_ENV = process.env.NODE_ENV
 global.DEVELOPMENT = NODE_ENV == 'development'
 global.PRODUCTION = NODE_ENV == 'production'
 
-
+console.info('process.env.NODE_ENV >')
+eyes.inspect(process.env.NODE_ENV)
 
 module.exports = {
 
 	outputDir: 'dist/client',
-	// dll: DEVELOPMENT, // faster incremental recompilation, slower initial build
+	dll: DEVELOPMENT, // faster incremental recompilation, slower initial build
 	css: { sourceMap: false }, // only enable when needed
-	// vueLoader: { hotReload: false }, // hot reload makes debugging difficult
+	vueLoader: { hotReload: false }, // hot reload makes debugging difficult
 
 	configureWebpack: function(config) {
 		config.entry.app = './src/client/main.ts'
@@ -39,26 +39,14 @@ module.exports = {
 	},
 
 	chainWebpack: function(config) {
-		config.plugin('define').tap(function(args) {
-			// apply package properties as env variables
-			args[0]['process.env'].NAME = `"${package.name}"`
-			args[0]['process.env'].VERSION = `"${package.version}"`
-			args[0]['process.env'].DOMAIN = `"${(DEVELOPMENT ? 'http://dev.' : 'https://') + package.domain}"`
-			// import variables defined in ./config/
-			let env = dotenv.config({ path: path.resolve(process.cwd(), 'config/client.env') }).parsed || {}
-			Object.assign(env, dotenv.config({ path: path.resolve(process.cwd(), 'config/client.' + NODE_ENV + '.env') }).parsed || {})
-			Object.keys(env).forEach(k => args[0]['process.env'][k] = `"${env[k]}"`)
-			return args
-		})
 		config.plugin('fork-ts-checker').tap(function(args) {
-			args[0].tsconfig = 'config/.client.tsconfig.json'
+			args[0].tsconfig = '.client.tsconfig.json'
 			args[0].workers = Math.ceil(require('os').cpus().length / 2)
 			return args
 		})
 		config.plugins.delete('no-emit-on-errors')
 		config.plugin('friendly-errors').tap(function(args) {
-			// don't clear my terminal/console... lol
-			args[0].clearConsole = false
+			args[0].clearConsole = false // don't clear my terminal/console
 			return args
 		})
 	},
